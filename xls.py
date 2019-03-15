fa3b38ddbac6a2277f11bd4061ce68d1b2315301
boy <boy@44886.com>
2019-03-15 20:48:13 +0800

    update

--- xls.py
+++ xls.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 # 44886
 # 学习作用，py操作xls
 # 一个简单的九九乘法表的生成
@@ -6,7 +7,6 @@ from openpyxl import Workbook
 wb = Workbook()  # 创建文件对象
 ws1 = wb.active  # 活跃的sheet
 ws1.title = "数据"
-
 
 
 # 思路：共9行，每行分别是1,2,3,4,5,6,7,8,9个内容
@@ -19,7 +19,7 @@ while foo <= 9:
     while i <= foo:
         row.append("{}x{}={}".format(i, foo, foo*i))
         i = i+1
-    ws1.append(row) #把刚刚那行，添加进表格中
+    ws1.append(row)  # 把刚刚那行，添加进表格中
     foo = foo+1
 
 # 生成文件

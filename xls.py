b9f7689966be8a7bf33bd6b7bacab0bdd796b20f
boy <boy@44886.com>
2019-03-15 20:43:51 +0800

    update

--- xls.py
+++ xls.py
@@ -7,14 +7,19 @@ wb = Workbook()  # 创建文件对象
 ws1 = wb.active  # 活跃的sheet
 ws1.title = "数据"
 
+
+
+# 思路：共9行，每行分别是1,2,3,4,5,6,7,8,9个内容
+# 用两层循环来进行
+
 foo = 1
 while foo <= 9:
     i = 1
-    row = []
+    row = []    # 用一个列表来存储 每一行的 i 个乘式
     while i <= foo:
         row.append("{}x{}={}".format(i, foo, foo*i))
         i = i+1
-    ws1.append(row)
+    ws1.append(row) #把刚刚那行，添加进表格中
     foo = foo+1
 
 # 生成文件
